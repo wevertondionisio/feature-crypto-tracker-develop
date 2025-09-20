6bafbf751a96d304565109c94df0fc9f10c82398
Weverton Dionisio <wevertondionisio@Wevertons-MacBook-Pro.local>
2025-09-20 13:14:31 -0300

    ajuste final

--- app/src/main/java/com/example/cryptotracker/MainActivity.kt
+++ app/src/main/java/com/example/cryptotracker/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 import com.example.cryptotracker.core.navigation.AdaptiveCoinListDetailPane
 import com.example.cryptotracker.ui.theme.CryptoTrackerTheme
 
